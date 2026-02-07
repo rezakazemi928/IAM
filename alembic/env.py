29a3c9fa038d8215bc657313d35ebae0f52a13ef
reza.kazemi <reza.kazemi@tapsi.cab>
2026-02-07 13:27:41 +0330

    update: db migrations

---
+++ alembic/env.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
+from alembic import context
+
+from app.core.config import settings
+from app.infrastructure.db.base import Base
+from app.infrastructure.db.models import account  # ensure models are imported
+
+config = context.config
+fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+def run_migrations_offline() -> None:
+    url = settings.DATABASE_URL
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online() -> None:
+    connectable = create_async_engine(settings.DATABASE_URL, poolclass=pool.NullPool)
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+    await connectable.dispose()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
--- app/infrastructure/db/models/account.py
+++ app/infrastructure/db/models/account.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 from app.domain.account_status import AccountStatus
 from app.infrastructure.db.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, Enum
 
 
 class AccountORM(Base):
@@ -12,10 +12,10 @@ class AccountORM(Base):
     
     id: Mapped[str] = mapped_column(String, primary_key=True, unique=True)
     client_identifier: Mapped[str] = mapped_column(String)
-    status:AccountStatus = mapped_column(String)
+    status: Mapped[AccountStatus] = mapped_column(Enum(AccountStatus))
     role_id: Mapped[str] = mapped_column(String)
     phone: Mapped[str] = mapped_column(String)
     token_key: Mapped[str] = mapped_column(String)
     password: Mapped[str] = mapped_column(String, nullable=True)
-    updated_at: datetime = mapped_column(DateTime, default=datetime.now(timezone.utc))
-    created_at: datetime = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
